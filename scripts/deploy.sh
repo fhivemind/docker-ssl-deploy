6bdb57ebefe5ee2d58809519c08fa4663ca6bab7
Ramiz Polić <32913827+fhivemind@users.noreply.github.com>
2019-08-12 22:11:26 +0200

    refactoring deployment scripts and templates

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,84 +1,88 @@
 #!/bin/bash
+set -eo pipefail
 
-# Boot nginx
-echo "[[ NGINX ]] Starting nginx..."
-docker-compose up --force-recreate -d nginx
-docker-compose exec -d nginx nginx -s reload
+#########################################
+# add vars
+CERT_PATH="./../certbot"
+NGINX_PATH="./../config/nginx"
+NGINX_TEMPLATE_PATH="./../config/nginx/app.tmpl"
+DOCKER_FILE="./../services/docker-compose.yml"
+rsa_key_size=4096
 
-# loop through config data
-for file in ./data/*; do
+#########################################
+# add warn messages
+succ ()  { echo $(tput bold)$(tput setaf 2) $@ $(tput sgr 0) ; }
+info ()  { echo $(tput bold)$(tput setaf 4) $@ $(tput sgr 0) ; }
+warn ()  { echo $(tput bold)$(tput setaf 3) $@ $(tput sgr 0) ; }
+error () { echo $(tput bold)$(tput setaf 1) $@ $(tput sgr 0) ; }
+
+#########################################
+# create functions
+create_dummy_certs() {
+  # variables
+  local HOST="$0"
+  local LIVE_PATH="/etc/letsencrypt/live/$HOST"
+  mkdir -p "$CERT_PATH/conf/live/$HOST"
   
-  # initialize data
-  source $file
-  filename="${file##*/}"
-  
-  rsa_key_size=4096
-  data_path="./certbot"
+  # run command
+  docker-compose -f "$DOCKER_FILE" run --rm --entrypoint "\
+    openssl req -x509 -nodes -newkey rsa:1024 -days 90\
+      -keyout '$LIVE_PATH/privkey.pem' \
+      -out '$LIVE_PATH/fullchain.pem' \
+      -subj '/CN=localhost'" certbot
+}
 
-  # if called for specific host
-  if [ -n "$1" ] && [ "$1" != "$APP_HOST" ]; then
-    continue
-  fi
-
-  # DOTS=${domains//[^.]};
-  # Generate nginx script from template for domains and subomains
-  # if  [[ ${#DOTS} > 1 ]]; then
-  #  echo "### Creating certificate template for subdomain $domains"
-  #  HOSTNAME="${domains#*.}"
-  #  envsubst "`printf '${%s} ' $(sh -c "env|cut -d'=' -f1")`" < ./nginx/app_sub.tmpl > "./nginx/$filename.conf"
-  #  continue
-  # else
-
-    # export variables
-    export APP_HOST="$APP_HOST" 
-    export APP_PORT="$APP_PORT" 
-    export APP_SERVICE="$APP_SERVICE"
-
-    envsubst "`printf '${%s} ' $(sh -c "env|cut -d'=' -f1")`" < ./nginx/app.tmpl > "./nginx/$filename.conf"
-  #fi
-  
-  if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-    echo "[[ STATUS ]] Downloading recommended TLS parameters..."
-    mkdir -p "$data_path/conf"
-    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
-    echo
-  fi
-
-  echo -e "==========================================================="
-  echo "[[ GENERATING CERTIFICATES ]]     $domains" 
-  echo -e "===========================================================\n"
-
-  # self-signed certificates gen
-  create_dummy_certs() {
-    echo -e $1
-    path="/etc/letsencrypt/live/$domains"
-    mkdir -p "$data_path/conf/live/$domains"
-    docker-compose run --rm --entrypoint "\
-      openssl req -x509 -nodes -newkey rsa:1024 -days 90\
-        -keyout '$path/privkey.pem' \
-        -out '$path/fullchain.pem' \
-        -subj '/CN=localhost'" certbot
-    echo
-  }
-
-  # generate initial certificates  
-  create_dummy_certs "[[ STATUS ]] Creating dummy certificates..."
-
+reload_nginx() {
   echo "[[ NGINX ]] Reloading nginx..."
-  docker-compose exec -d nginx nginx -s reload
+  docker-compose -f $DOCKER_FILE exec -d nginx nginx -s reload
+}
+
+start_ngingx() {
+  echo "[[ NGINX ]] Starting nginx..."
+  docker-compose -f $DOCKER_FILE up --force-recreate -d nginx
+}
+
+create_valid_certs() {
+  # export variables
+  local APP_HOST="$1" 
+  local APP_PORT="$2" 
+  local APP_SERVICE="$3"
+  local USER_EMAIL="$4"
+  local STAGING="$5"
+  local DOMAINS="$6"
+
+  # create nginx conf file 
+  conf_file="$NGINX_PATH/$APP_SERVICE.conf"
+  
+  cp $NGINX_TEMPLATE_PATH $conf_file
+  sed -i "s/APP_HOST/$APP_HOST/g" $conf_file
+  sed -i "s/APP_PORT/$APP_PORT/g" $conf_file
+  sed -i "s/APP_SERVICE/$APP_SERVICE/g" $conf_file
+
+  # get TLS data
+  if [ ! -e "$CERT_PATH/conf/options-ssl-nginx.conf" ] || [ ! -e "$CERT_PATH/conf/ssl-dhparams.pem" ]; then
+    echo "[[ STATUS ]] Downloading recommended TLS parameters..."
+    mkdir -p "$CERT_PATH/conf"
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/options-ssl-nginx.conf > "$CERT_PATH/conf/options-ssl-nginx.conf"
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > "$CERT_PATH/conf/ssl-dhparams.pem"
+    echo
+  fi
+
+  # create local certs
+  create_dummy_certs "$APP_HOST"
+  reload_nginx
 
   # delete dummy certificates
   echo "[[ STATUS ]] Deleting dummy certificate..."
-  docker-compose run --rm --entrypoint "\
-    rm -Rf /etc/letsencrypt/live/$domains && \
-    rm -Rf /etc/letsencrypt/archive/$domains && \
-    rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+  docker-compose -f $DOCKER_FILE run --rm --entrypoint "\
+    rm -Rf /etc/letsencrypt/live/$APP_HOST && \
+    rm -Rf /etc/letsencrypt/archive/$APP_HOST && \
+    rm -Rf /etc/letsencrypt/renewal/$APP_HOST.conf" certbot
 
   # Create LetsEncrypt certificates
-  echo -e "[[ STATUS ]] Requesting Let's Encrypt certificate for $domains..."
+  echo -e "[[ STATUS ]] Requesting Let's Encrypt certificate for $DOMAINS..."
   domain_args=""
-  for domain in "${domains[@]}"; do
+  for domain in "${DOMAINS[@]}"; do
     domain_args="$domain_args -d $domain"
   done
 
@@ -89,10 +93,12 @@ for file in ./data/*; do
   esac
 
   # Enable staging if needed
-  if [ $staging != "0" ]; then staging_arg="--staging"; fi
+  if [[ $staging != "0" ]]; then 
+    staging_arg="--staging"; 
+  fi
 
-  # Obtain LE cert
-  docker-compose run --rm --entrypoint "\
+  # install certificates
+  docker-compose -f $DOCKER_FILE run --rm --entrypoint "\
     certbot certonly --webroot -w /var/www/certbot \
       $staging_arg \
       $email_arg \
@@ -101,11 +107,11 @@ for file in ./data/*; do
       --agree-tos \
       --force-renewal" certbot \
       && echo -e "\n[[SUCCESS]] {$domain}\n" \
-      || create_dummy_certs "\n[[FAILURE]] Restoring previous self-signed certificates.\n"
+      || create_dummy_certs "$APP_HOST"
 
-  echo
-done
+  # reload nginx
+  reload_nginx
+}
 
-# Reboot nginx
-echo "[[ NGINX ]] Reloading nginx..."
-docker-compose exec -d nginx nginx -s reload
+
+#create_valid_certs "example.com" "3000" "example-service" "rase@mail.com" "0" "domain.com www.domain.com nesto.nesto.com"
