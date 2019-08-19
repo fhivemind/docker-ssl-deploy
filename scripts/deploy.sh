06f08fa78f2df887f08cea8b7ae9f5a60ef4f72a
Ramiz Polić <32913827+fhivemind@users.noreply.github.com>
2019-08-19 17:41:31 +0200

    implementing runner script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,16 +10,62 @@ DOCKER_FILE="./../services/docker-compose.yml"
 rsa_key_size=4096
 
 #########################################
+# load input vars
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -h|--host)
+    APP_HOST="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -p|--port)
+    APP_PORT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -s|--service)
+    APP_SERVICE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -e|--email)
+    USER_EMAIL="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --staging)
+    STAGING=1
+    shift # past argument
+    ;;
+    -d|--domains)
+    DOMAINS="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+#########################################
 # add warn messages
-succ ()  { echo $(tput bold)$(tput setaf 2) $@ $(tput sgr 0) ; }
-info ()  { echo $(tput bold)$(tput setaf 4) $@ $(tput sgr 0) ; }
-warn ()  { echo $(tput bold)$(tput setaf 3) $@ $(tput sgr 0) ; }
-error () { echo $(tput bold)$(tput setaf 1) $@ $(tput sgr 0) ; }
+succ ()  { echo -e "  " - $(tput bold)$(tput setaf 2) $@ $(tput sgr 0) ; }
+info ()  { echo -e "  " /-- $(tput bold)$(tput setaf 4) $@ $(tput sgr 0) ; }
+warn ()  { echo -e !-- $(tput bold)$(tput setaf 3) $@ $(tput sgr 0) ; }
+error () { echo -e !!! $(tput bold)$(tput setaf 1) $@ $(tput sgr 0) ; }
 
 #########################################
 # create functions
 create_dummy_certs() {
   # variables
+  warn "Creating dummy certificates..."
   local HOST="$0"
   local LIVE_PATH="/etc/letsencrypt/live/$HOST"
   mkdir -p "$CERT_PATH/conf/live/$HOST"
@@ -33,24 +79,16 @@ create_dummy_certs() {
 }
 
 reload_nginx() {
-  echo "[[ NGINX ]] Reloading nginx..."
+  info "Reloading nginx..."
   docker-compose -f $DOCKER_FILE exec -d nginx nginx -s reload
 }
 
 start_ngingx() {
-  echo "[[ NGINX ]] Starting nginx..."
+  warn "Starting nginx..."
   docker-compose -f $DOCKER_FILE up --force-recreate -d nginx
 }
 
 create_valid_certs() {
-  # export variables
-  local APP_HOST="$1" 
-  local APP_PORT="$2" 
-  local APP_SERVICE="$3"
-  local USER_EMAIL="$4"
-  local STAGING="$5"
-  local DOMAINS="$6"
-
   # create nginx conf file 
   conf_file="$NGINX_PATH/$APP_SERVICE.conf"
   
@@ -61,11 +99,10 @@ create_valid_certs() {
 
   # get TLS data
   if [ ! -e "$CERT_PATH/conf/options-ssl-nginx.conf" ] || [ ! -e "$CERT_PATH/conf/ssl-dhparams.pem" ]; then
-    echo "[[ STATUS ]] Downloading recommended TLS parameters..."
+    info "Downloading recommended TLS parameters..."
     mkdir -p "$CERT_PATH/conf"
     curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/options-ssl-nginx.conf > "$CERT_PATH/conf/options-ssl-nginx.conf"
     curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > "$CERT_PATH/conf/ssl-dhparams.pem"
-    echo
   fi
 
   # create local certs
@@ -73,30 +110,29 @@ create_valid_certs() {
   reload_nginx
 
   # delete dummy certificates
-  echo "[[ STATUS ]] Deleting dummy certificate..."
+  info "Deleting dummy certificate..."
   docker-compose -f $DOCKER_FILE run --rm --entrypoint "\
     rm -Rf /etc/letsencrypt/live/$APP_HOST && \
     rm -Rf /etc/letsencrypt/archive/$APP_HOST && \
     rm -Rf /etc/letsencrypt/renewal/$APP_HOST.conf" certbot
 
   # Create LetsEncrypt certificates
-  echo -e "[[ STATUS ]] Requesting Let's Encrypt certificate for $DOMAINS..."
+  warn "Requesting Let's Encrypt certificate for '$APP_HOST'" 
   domain_args=""
   for domain in "${DOMAINS[@]}"; do
     domain_args="$domain_args -d $domain"
   done
 
   # Email arg
-  case "$email" in
+  case "$USER_EMAIL" in
     "") email_arg="--register-unsafely-without-email" ;;
-    *) email_arg="--email $email" ;;
+    *) email_arg="--email $USER_EMAIL" ;;
   esac
 
   # Enable staging if needed
-  if [[ $staging != "0" ]]; then 
+  if [[ $STAGING == "1" ]]; then 
     staging_arg="--staging"; 
   fi
-
   # install certificates
   docker-compose -f $DOCKER_FILE run --rm --entrypoint "\
     certbot certonly --webroot -w /var/www/certbot \
@@ -106,12 +142,16 @@ create_valid_certs() {
       --rsa-key-size $rsa_key_size \
       --agree-tos \
       --force-renewal" certbot \
-      && echo -e "\n[[SUCCESS]] {$domain}\n" \
+      && succ "Success for {$domain}\n" \
       || create_dummy_certs "$APP_HOST"
 
   # reload nginx
   reload_nginx
+  echo
 }
 
-
-#create_valid_certs "example.com" "3000" "example-service" "rase@mail.com" "0" "domain.com www.domain.com nesto.nesto.com"
+#########################################
+# run script
+echo "=================== ${APP_HOST} ==================="
+start_ngingx
+create_valid_certs
